2850cf8734d4b4a34ce4ea05588b31c14102eaf8
Tim Keane <tkeane@doitt.nyc.gov>
2019-01-03 17:00:48 -0500

    Update springs.js

--- springs/js/springs.js
+++ springs/js/springs.js
@@ -179,5 +179,5 @@ new nyc.ol.FeatureTip({
   }]
 });
 
-$('.ol-overlaycontainer-stopevent'). append('#menu');
+$('.ol-overlaycontainer-stopevent').append($('#menu'));
 $('#menu button').trigger('click');
